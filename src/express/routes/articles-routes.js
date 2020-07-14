09fcb2e18749eac4c4b1707a0fa8e5e3841d17eb
bgutsol <bogdan.gutsol@gmail.com>
2020-07-14 23:28:01 +0300

    feat: add templates, edit routes resolving

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
+articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`new-post`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 
 module.exports = articlesRouter;
